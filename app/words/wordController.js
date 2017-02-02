54a57df02b19a569dcda63aa030e3b8f0b5d0d5e
svcoding4 <svcoding4@svcoding4ui-Mac-mini.local>
2017-02-02 16:26:41 +0900

    add word array

--- app/words/wordController.js
+++ app/words/wordController.js
@@ -5,6 +5,7 @@ var Word = require('./wordModel.js');
 // Promisify a few mongoose methods with the `q` promise library
 var findWord = Q.nbind(Word.findOne, Word);
 var createWord = Q.nbind(Word.create, Word);
+var removeWords = Q.nbind(Word.remove, Word);
 
 module.exports = {
 
@@ -19,6 +20,7 @@ module.exports = {
 		  console.log('readFile length = ', words.length );
 
 		  var newWord = {};
+		  var wordArr = [];
 
 		  for(let i = 0; i < words.length; i++ ){
 		  	var dic = words[i].split('#');
@@ -28,25 +30,46 @@ module.exports = {
 		    newWord['definition'] = dic[2];
 		    newWord['example'] = dic[3];
 		    newWord['category'] = dic[4];
-		  }
-		  console.log('the last line = ', newWord );
 
-		  findWord({korean:newWord.korean})
-      .then(function(word) {
-      	if(word) {
-      		console.log('word already exist!');
-          res.json('word already exist!');
-        } 
-        else {
-          // make a new word if not one
-          res.json( createWord(newWord) );
-        }
+		    wordArr.push(newWord);
+		  }
+		  
+		  console.log('the last line = ', wordArr.length );
+
+      removeWords({})
+        .then(function(result){
+          console.log(result);
+          
+          createWord( wordArr )
+			    .then(function(result){
+	          res.json( result.length );
+			    })
+			    .fail(function(err){
+	          res.json( error );
+			    });
+        })
+        .fail(function(err){
+        	res.json( error );
+        });
+		  
+
+		  // findWord({korean:newWord.korean})
+    //   .then(function(word) {
+    //   	if(word) {
+    //   		console.log('word already exist!');
+    //       res.json('word already exist!');
+    //     } 
+    //     else {
+    //       // make a new word if not one
+    //       res.json( createWord(newWord) );
+    //     }
       	
-      })
-      .fail(function (error) {
-      	// respond to the client
-		    res.json( error );
-      });
+    //   })
+    //   .fail(function (error) {
+    //   	// respond to the client
+		  //   res.json( error );
+    //   });
+
 		});  
     
   }

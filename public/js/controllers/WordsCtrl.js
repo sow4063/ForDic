05f5defaeea5e376c19888101dce730554ed35c3
sow4063 <sow4063@gmail.com>
2017-02-03 18:46:38 +0900

    search::waiting message

--- public/js/controllers/WordsCtrl.js
+++ public/js/controllers/WordsCtrl.js
@@ -1,12 +1,16 @@
 angular.module('WordsCtrl', [])
 .controller('WordsController', function($scope, Words) {
 
+  $scope.busy = false;
   $scope.words = [];
   $scope.where = 'korean';
 
 	$scope.search = function (text) {
+		$scope.busy = true;
+
 		Words.search(text, $scope.where)
 			.then(function (data) {
+				$scope.busy = false;
 				$scope.words = data;
 			});
 	};

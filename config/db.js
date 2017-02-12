0c323e9429103a4a14b4f1ed5721c51f03c8d266
sow4063 <sow4063@gmail.com>
2017-02-12 20:32:30 +0900

    add ServerSpec.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,12 @@
+var mongoose = require('mongoose');
+
+var url = 'mongodb://localhost/dic';
+
+// // connect to our mongoDB database (commented out after you enter in your own credentials)
+// var db = mongoose.connect(url);
+
 module.exports = {
-	url : 'mongodb://localhost/dic'
-}
+	url: 'mongodb://localhost/dic',
+	db: mongoose.connect(url)
+};
+
---
+++ test/ServerSpec.js
@@ -0,0 +1,40 @@
+var request = require('supertest');
+var express = require('express');
+var expect = require('chai').expect;
+var app = require('../server.js');
+var mongoose = require('mongoose');
+
+var db = require('../config/db.js');
+var Word = require('../app/words/wordModel.js');
+var WordController = require('../app/words/wordController.js');
+
+/////////////////////////////////////////////////////
+// NOTE: these tests are designed for mongo!
+/////////////////////////////////////////////////////
+
+describe('', function() {
+
+	beforeEach(function(done) {
+		if( mongoose.connection.db ) {
+			console.log('already connected.');
+			return done();
+		}
+
+    console.log('db connect.');
+    mongoose.connect(db.url, done);
+  });
+
+  describe('word creation: ', function() {
+
+    it('Only valid urls, returning a 404 - Not found for invalid urls', function(done) {
+      request(app)
+        .post('/links')
+        .send({
+          'url': 'definitely not a valid url'})
+        .expect(404)
+        .end(done);
+    });
+
+  });
+
+});
